582d0879471e842d2393e83eb9541c7613aff80c
Alexandre Lima <limaalexguitar@gmail.com>
2021-01-31 09:48:24 -0300

    Testando o og:image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import Head from 'next/head'
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -16,16 +18,39 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
-
 export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Quiz JavaScript - AluraQuiz</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+
+        {/* <!-- Primary Meta Tags --> */}
+        <title>JavaScript Quiz</title>
+        <meta name="title" content="JavaScript Quiz" />
+        <meta name="description" content="" />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://alura-quiz.alexandresl.vercel.app/" />
+        <meta property="og:title" content="JavaScript Quiz" />
+        <meta property="og:description" content="Feito durante a Imersão Alura Quiz" />
+        <meta property="og:image" content={db.bg} />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://alura-quiz.alexandresl.vercel.app/" />
+        <meta property="twitter:title" content="JavaScript Quiz" />
+        <meta property="twitter:description" content="Feito durante a Imersão Alura Quiz" />
+        <meta property="twitter:image" content={db.bg}></meta>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
-            <Widget.Header>
-              <h1>JavaScript Quiz</h1>
-            </Widget.Header>
+          <Widget.Header>
+            <h1>JavaScript Quiz</h1>
+          </Widget.Header>
           <Widget.Content>
             <p>Teste seus conhecimentos sobre o universo Javascript, incluindo React, Node.JS, Next.JS, etx...</p>
           </Widget.Content>
