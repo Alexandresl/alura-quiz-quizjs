ffda0bcdcc30a964f8773044bea6f7c0a0e4275a
Alexandre Lima <limaalexguitar@gmail.com>
2021-01-30 21:39:06 -0300

    Comitando o background

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components'
+import db from '../db.json';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-Image: url("../js-min.jpg");
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      alskjdf
+    </BackgroundImage>
+  );
 }
+ 
